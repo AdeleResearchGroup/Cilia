5086bdd213d9558257fca17f2a654952f0c32fe7
Morand <denis.morand@imag.fr>
2012-07-05 11:55:45 +0200

    unused directories are removed

--- src/core/src/main/java/fr/liglab/adele/cilia/ModelComponents.java
+++ src/core/src/main/java/fr/liglab/adele/cilia/ModelComponents.java
@@ -14,6 +14,7 @@ import fr.liglab.adele.cilia.model.MediatorComponent;
  *         Team</a>
  * 
  */
+@SuppressWarnings("rawtypes")
 public interface ModelComponents {
 	/**
 	 * Return all properties for a node 
