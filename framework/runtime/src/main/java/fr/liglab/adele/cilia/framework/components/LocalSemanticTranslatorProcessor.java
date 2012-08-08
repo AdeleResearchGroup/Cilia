e9a2c53c0afbb47787ec7506fbf745a72c111d93
Issac Noé <torito@gmail.com>
2012-08-08 17:33:18 +0200

    Change List to Data in Translator

--- framework/runtime/src/main/java/fr/liglab/adele/cilia/framework/components/LocalSemanticTranslatorProcessor.java
+++ framework/runtime/src/main/java/fr/liglab/adele/cilia/framework/components/LocalSemanticTranslatorProcessor.java
@@ -1,34 +1,58 @@
+/*
+ * Copyright Adele Team LIG
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package fr.liglab.adele.cilia.framework.components;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.liglab.adele.cilia.Data;
-import fr.liglab.adele.cilia.framework.IProcessor;
-
-public class LocalSemanticTranslatorProcessor implements IProcessor{
-
-	private Map dictionary;
-
-	public List process(List dataSet) {
+/**
+ * 
+ * @author <a href="mailto:cilia-devel@lists.ligforge.imag.fr">Cilia Project
+ *         Team</a>
+ *
+ */
+public class LocalSemanticTranslatorProcessor {
+	/**
+	 * The dictionary containing words to be changed.
+	 */
+	private Map<String, String> dictionary;
+	
+	private static final Logger logger = LoggerFactory.getLogger("cilia.components");
+	/**
+	 * Will translate words on the content of a data.
+	 * @param dataSet The data to be modified.
+	 * @return The same data with the modified content.
+	 */
+	public Data process(Data dataSet) {
 		String translatedContent;
-		Iterator it;
-		/**
-		 * pour toute les donn�es
-		 */
-		for(int i=0;i<dataSet.size();i++){
-			translatedContent=(String) ((Data)dataSet.get(i)).getContent();
-			/**
-			 * Remplacement des occurences des entr�e du dictionaire;
-			 */
-			 it = dictionary.entrySet().iterator();
-		    while (it.hasNext()) {
-		        Map.Entry pairs = (Map.Entry)it.next();
-		        translatedContent.replaceAll((String)pairs.getKey(),(String)pairs.getValue());
-		    }
-		    ((Data)dataSet.get(i)).setContent(translatedContent);
+		if (dictionary == null) { // If any configuration has been given, it returns the same content.
+			logger.warn("Translator will return the same received Data. Dictionary is null");
+			return dataSet;
 		}
+		translatedContent=(String) dataSet.getContent();
+		Iterator<Entry<String, String>> it = dictionary.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, String> pairs = it.next();
+			translatedContent = translatedContent.replaceAll(pairs.getKey(),pairs.getValue());
+		}
+		dataSet.setContent(translatedContent);
 		
 		return dataSet;
 	}
