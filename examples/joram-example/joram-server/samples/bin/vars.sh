6ecb363f9d66bc0f955298bb8000c7a88b020a9d
torito <torito@gmail.com>
2013-02-19 15:18:40 +0100

    Add monitoring_habits example from M2MAppBuilder, but with a distributed chain

--- examples/joram-example/joram-server/samples/bin/vars.sh
+++ examples/joram-example/joram-server/samples/bin/vars.sh
@@ -1,5 +1,5 @@
 #export JORAM_HOME="/home/torito/Documents/workspaces/joram"
-export JORAM_HOME="$PWD/../../"
+export set JORAM_HOME="$PWD/../../"
 
-export JAVA_HOME="/usr/lib/jvm/java-6-openjdk/"
+export set JAVA_HOME="/usr/lib/jvm/java-6-openjdk/"
 
