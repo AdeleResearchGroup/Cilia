455bbe5288a7820cc6c3a51f2ebf6793bbd3b5ad
torito <torito@gmail.com>
2013-09-25 14:52:44 +0200

    remove wrong/unused files

--- examples/hello-world-example-annotations/gateway/start.bat
+++
@@ -1,2 +0,0 @@
-cd "/home/torito/workspace/Cilia/examples/hello-world-example-annotations/gateway/target/annotated-hello-world-distribution"
-java %* -jar bin\felix.jar
--- examples/hello-world-example-annotations/gateway/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-cd "/home/torito/workspace/Cilia/examples/hello-world-example-annotations/gateway/target/annotated-hello-world-distribution"
-exec java $@ -jar bin/felix.jar
